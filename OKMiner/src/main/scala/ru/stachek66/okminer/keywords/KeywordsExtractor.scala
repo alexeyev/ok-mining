3b8de53a90ab3f2556dcb1aa7221ae90c6f90f8e
alexeyev <anton.m.alexeyev@gmail.com>
2013-11-18 13:51:43 +0400

    Removed stemmer, test modified.

--- OKMiner/src/main/scala/ru/stachek66/okminer/keywords/KeywordsExtractor.scala
+++ OKMiner/src/main/scala/ru/stachek66/okminer/keywords/KeywordsExtractor.scala
@@ -1,9 +1,10 @@
 package ru.stachek66.okminer.keywords
 
+import java.io.File
 import org.apache.commons.collections.bag.HashBag
-import ru.stachek66.okminer.corpus.CorpusStats
 import ru.stachek66.okminer.language.russian.StopWordsFilter
 import ru.stachek66.okminer.language.russian.Tokenizer._
+import ru.stachek66.okminer.utils.FileUtils
 import scala.collection.JavaConversions._
 
 
@@ -38,12 +39,11 @@ object Test {
     //    println(CorpusStats.docsInCorpus)
     //println(CorpusStats.bags)
 
-//    println(CorpusStats.docsInCorpus("скачаю"))
+    //    println(CorpusStats.docsInCorpus("скачаю"))
 
     println(new KeywordsExtractor().getRanked(
-      "«Зенит» U-17 обыграл «Крылья Советов»\nКоманда Академии 1996 года рождения с победы начала выступление на первенстве России среди юношеских команд клубов Премьер-лиги и ФНЛ.\n\nВ стартовом матче группового этапа первенства подопечные Александра Селенкова и Евгения Тарасова обыграли самарские «Крылья Советов» со счетом 2:1. Голами в составе петербуржцев отметились Павел Долгов и Павел Назимов.\n\nСледующий матч на турнире зенитовцы проведут 16 октября против пермского «Амкара». ",
+      FileUtils.asStringWithoutNewLines(new File("corpus/9.txt")),
       new BasicTfIdf
     ))
-
   }
 }
