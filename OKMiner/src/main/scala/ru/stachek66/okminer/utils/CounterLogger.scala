e3abaca0048e41db0ba83ebc5db144a64e3d9839
alexeyev <anton.m.alexeyev@gmail.com>
2014-01-08 02:37:57 +0400

    Minor improvements

---
+++ OKMiner/src/main/scala/ru/stachek66/okminer/utils/CounterLogger.scala
@@ -0,0 +1,33 @@
+package ru.stachek66.okminer.utils
+
+import org.slf4j.Logger
+
+/**
+ * @author alexeyev
+ */
+class CounterLogger(log: Logger, step: Int, pattern: String) {
+  //todo: write in scala-style, without ticks
+  if (step < 1) throw new IllegalArgumentException("Step must be a positive integer.")
+
+  def tick() {
+    counter += 1
+    if (counter % step == 0) {
+      log.info(pattern.format(counter))
+      prevCounter = counter
+    }
+  }
+
+  def tick(up: Int) {
+    counter += up
+    if (counter - prevCounter >= step) {
+      log.info(pattern.format(counter))
+      prevCounter = counter
+    }
+  }
+
+  @volatile
+  private var counter: Long = 0
+  @volatile
+  private var prevCounter: Long = 0
+}
+
--- OKMiner/src/main/scala/ru/stachek66/okminer/wiki/WikiParser.scala
+++ OKMiner/src/main/scala/ru/stachek66/okminer/wiki/WikiParser.scala
@@ -3,21 +3,31 @@ package ru.stachek66.okminer.wiki
 import edu.jhu.nlp.wikipedia.{WikiPage, PageCallbackHandler, WikiXMLParserFactory}
 import org.slf4j.LoggerFactory
 import scala.collection.JavaConversions._
+import java.io.FileWriter
+import ru.stachek66.okminer.utils.CounterLogger
 
 /**
  * @author alexeyev
  */
 object WikiParser extends App {
   private val log = LoggerFactory.getLogger(this.getClass)
+  private val clog = new CounterLogger(log, 1000, "%s articles processed")
+
   val wxsp = WikiXMLParserFactory.getSAXParser(
     "../ruwiki-latest-pages-articles-multistream.xml")
+  val writer = new FileWriter("wiki-links-russian-from-parser.txt")
 
   try {
     wxsp.setPageCallback(
       new PageCallbackHandler {
         def process(page: WikiPage) {
-          if (page.getTranslatedTitle("en") != null)
-            log.info(" " + page.getTranslatedTitle("en") +" " + page.getID + " " + page.getLinks)
+          clog.tick()
+          if (page.getLinks.nonEmpty)
+                                                         //          if (
+          // page.getTranslatedTitle("en") != null)
+          //            log.info(" " + page.getTranslatedTitle("en") +" " + page.getID + " " + page.getLinks)
+            writer.write("%s;%s\n".format(page.getTitle.trim, page.getLinks.map("\"%s\"".format(_)).mkString(",")))
+
         }
       }
     )
@@ -26,4 +36,5 @@ object WikiParser extends App {
     case e: Exception =>
       log.error("Oops", e)
   }
+  writer.close()
 }
