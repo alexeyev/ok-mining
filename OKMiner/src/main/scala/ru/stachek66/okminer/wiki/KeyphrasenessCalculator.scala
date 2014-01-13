2ca0a913ff3cc56b49c653f0130a7f02d7809cd7
alexeyev <anton.m.alexeyev@gmail.com>
2014-01-14 00:40:51 +0400

    Vocabulary file added

---
+++ OKMiner/src/main/scala/ru/stachek66/okminer/wiki/KeyphrasenessCalculator.scala
@@ -0,0 +1,15 @@
+package ru.stachek66.okminer.wiki
+
+import org.slf4j.LoggerFactory
+
+/**
+ * @author alexeyev
+ */
+object KeyphrasenessCalculator {
+
+  def getKeyPhraseness(phrase: String): Double = {
+    val keys = keyphrases.Searcher.tryPhrase(phrase)
+    val all = articles.Searcher.tryPhrase(phrase)
+    keys.size / (all.size + 0.01)
+  }
+}
