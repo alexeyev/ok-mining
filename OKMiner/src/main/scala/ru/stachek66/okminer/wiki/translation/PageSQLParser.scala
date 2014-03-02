bbd338078df984daa8d80918cad3fd2714ecb690
alexeyev <anton.m.alexeyev@gmail.com>
2014-03-02 21:34:32 +0400

    Lightweight parser

--- OKMiner/src/main/scala/ru/stachek66/okminer/wiki/translation/PageSQLParser.scala
+++ OKMiner/src/main/scala/ru/stachek66/okminer/wiki/translation/PageSQLParser.scala
@@ -1,8 +1,11 @@
 package ru.stachek66.okminer.wiki.translation
 
-import java.io.{FileWriter, FileReader, BufferedReader, File}
+import java.io._
 import org.slf4j.LoggerFactory
 import scala.util.{Success, Failure, Try}
+import scala.util.Success
+import scala.util.Failure
+import java.util.zip.GZIPInputStream
 
 /**
  * Parses id-to-title mapping for Russian wiki-articles
@@ -12,17 +15,23 @@ object PageSQLParser {
 
   private val log = LoggerFactory.getLogger("")
 
-  private val rudump = new File("../ruwiki-latest-page.sql")
-  private val endump = new File("../enwiki-latest-page.sql")
+  private val rudump = new File("../ruwiki-latest-page.sql.gz")
+  private val endump = new File("../enwiki-latest-page.sql.gz")
+
+
+  private val ruPrepared = new File("parsed/ru-id-title.tsv")
+  private val enPrepared = new File("parsed/en-id-title.tsv")
+
+  //todo: strange stuff, make simple
+  private val ruPreparedStream = new FileInputStream(ruPrepared)
+  private val enPreparedStream = new FileInputStream(enPrepared)
+
   private val pattern = "\\((\\d+),\\d+,'([^']+)'".r
 
-  def parseFile(dump: File, enc: String): (Map[Long, String], Map[String, Long]) = {
-
-    val idToTitleMap = collection.mutable.Map[Long, String]()
-    val titleToIdMap = collection.mutable.Map[String, Long]()
+  private[translation] def parseDump(dump: File, enc: String)(handler: (Long, String) => Unit = ()) {
 
     // due to malformed input, we have to use buffered reader  :(
-    val br = new BufferedReader(new FileReader(dump))
+    val br = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(dump))))
     var c = 0
 
     while (br.ready()) Try {
@@ -34,36 +43,58 @@ object PageSQLParser {
             val spl = m.group(1).split(",")
             val id = spl(0).toLong
             val tal = m.group(2).replace("_", " ")
-            titleToIdMap.put(tal, id)
-            idToTitleMap.put(id, tal)
+            handler(id, tal)
           })
       }
     } match {
       case Failure(e) => log.error("", e)
       case Success(s) => log.info("Line " + c)
     }
-    log.info("Converting to maps... " + idToTitleMap.size + " " + titleToIdMap.size)
-    (idToTitleMap.toMap, titleToIdMap.toMap)
+  }
+
+  private def getMapsFromTsv(stream: InputStream): (Map[Long, String], Map[String, Long]) = {
+
+    val itt = collection.mutable.Map[Long, String]()
+    val tti = collection.mutable.Map[String, Long]()
+
+    io.Source.fromInputStream(stream).getLines().foreach {
+      line => {
+        val splitted = line.trim.split("\t")
+        val number = splitted(0).toLong
+        itt.put(number, splitted(1))
+        tti.put(splitted(1), number)
+      }
+    }
+
+    (itt.toMap, tti.toMap)
   }
 
   lazy val (ruIdToTitle, ruTitleToId): (Map[Long, String], Map[String, Long]) = {
-    log.info("Does ru-dump exist? " + rudump.exists())
-    if (rudump.exists()) parseFile(rudump, "utf8")
-    else (Map(), Map())
+    if (ruPreparedStream.available() != 1) {
+      flushRu()
+    }
+    getMapsFromTsv(ruPreparedStream)
   }
 
   lazy val (enIdToTitle, enTitleToId): (Map[Long, String], Map[String, Long]) = {
-    log.info("Does en-dump exist? " + endump.exists())
-    if (endump.exists()) parseFile(endump, "utf8")
-    else (Map(), Map())
+    if (enPreparedStream.available() != 1) {
+      flushEn()
+    }
+    getMapsFromTsv(enPreparedStream)
   }
 
-  def flush() {
-    val fw = new FileWriter("parsed/ru-id-title.tsv")
-    for {
-      (title, id) <- ruIdToTitle
-    } {
-      fw.write("%s\t%s\n".format(id, title))
+  private def flushRu() {
+    val fw = new FileWriter(ruPrepared)
+    parseDump(rudump, "utf8") {
+      (id, title) => fw.write("%s\t%s\n".format(id, title))
+    }
+    fw.close()
+  }
+
+  private def flushEn() {
+    val fw = new FileWriter(enPrepared)
+    parseDump(endump, "utf8") {
+      (id, title) => fw.write("%s\t%s\n".format(id, title))
     }
     fw.close()
   }
